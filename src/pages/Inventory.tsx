ff648901d387c2edcaa04f53081b439ae4417ee6
abdouni493 <youssefabdouni44@gmail.com>
2025-09-24 19:12:11 +0200

    Update Inventory.tsx

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -55,7 +55,7 @@ const allCategories: Record<string, { fr: string; ar: string }> = {
   safety_ppe: { fr: "Sécurité & EPI", ar: "معدات الحماية الشخصية" },
 };
 
-const API = "http://localhost:5000/api/products";
+const API = " /api/products";
 
 export default function Inventory() {
   const { toast } = useToast();

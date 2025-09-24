80ac83a40f425d7bd638b7e75bcdb702489ff37c
abdouni493 <youssefabdouni44@gmail.com>
2025-09-24 19:18:24 +0200

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import {
+import { 
   Eye,
   EyeOff,
   Save,
